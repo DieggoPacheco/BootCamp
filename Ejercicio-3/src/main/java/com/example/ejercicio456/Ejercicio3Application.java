a53a640cf872b13bce3c66e2923b8f466fdd8466
DieggoPacheco <dapachecopa@ufpso.edu.co>
2023-04-02 09:44:27 -0500

    ejercicios 1,2,3,4,5,6

---
+++ Ejercicio-3/src/main/java/com/example/ejercicio456/Ejercicio3Application.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ejercicio456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ejercicio456Application.class, args);
+	}
+
+}
